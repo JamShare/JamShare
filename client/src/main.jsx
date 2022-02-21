0f2eef3a2495d87325b5f4fadcb5b96ce60c8151
Qazzzie <santangelozach@gmail.com>
2022-02-21 09:00:13 -0800

    more skeleton code. GET requests successfully log and display request information.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+//start here: https://reactjs.org/docs/introducing-jsx.html
+
 /**
  * 
  * @param {socket Object} socket - The socket object that user used to connect to server. 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-const express = require('express');
+const express = require('express');//http://expressjs.com/en/5x/api.html
 const { createServer } = require('http');
 const { Server } = require("socket.io");
-
 const PORT = 3000;
 // var mongoose = require('mongoose');
 
@@ -9,31 +8,59 @@ const PORT = 3000;
 // const AudioContext = window.AudioContext || window.webkitAudioContext; //https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
 // const audioContext = new AudioContext(); //https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
 
+const{
+
+} = require("./audio.js")
+
+//sessions
+const{
+    // joinSession,
+} = require("./session.js");
+
+// database
+// const{
+
+// } = require("./database.js");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server (httpServer, { /* options */});
+const io = new Server (httpServer, { /* options */}); // using socket with express middleware: https://socket.io/docs/v4/server-initialization/
+
+
+// session ID generation https://socket.io/docs/v4/server-instance/
+// const uuid = require("uuid");
+// io.engine.generateId = (req) => {  return uuid.v4(); }// must be unique across all Socket.IO servers
+
+//customizing initial headers on request ("initial_headers", "headers", and "connection_error" are specially emitted by socket.io)
+// io.engine.on("initial_headers", (headers, req) => {  
+//     headers["JamShare"] = "JamShare";  
+//     headers["set-cookie"] = "mycookie=JamShare";
+// });
 //environments
 
+// allowing express to load external resources
 // app.use('/cors', require('cors')());
 
 // socket events
 io.on("connection", (socket) => {
-    console.log("Client Connected");
+    let clientID = socket.id;
+    console.log("Client Connected", clientID);
     
     // add server actions here
-    socket.on("join-session", (sessionID) => joinSession(sessionID));
+    //sessions
+    socket.on("join-session", (sessionID) => joinSession(clientID, sessionID));
 
-    // recieve data from a user
-    try{
-        
-    } catch (error) {
-        console.error(error);
-    }
+    //database
 
-    // server action functions
-    
+    //serverside audio
 
+});
+
+io.engine.on("connection_error", (err) => {
+      console.log(err.req);      // the request object  
+      console.log(err.code);     // the error code, for example 1  
+      console.log(err.message);  // the error message, for example "Session ID unknown"  
+      console.log(err.context);  // some additional error context
 });
 
 //set listen port, and log it.
@@ -46,10 +73,11 @@ httpServer.listen(PORT, () => {
 app.use((req, res, next) => {
     // For example, a GET request to `/test` will print "GET /test"
     console.log(`${req.method} ${req.url}`);
-  
+    res.send(`${req.method} ${req.url}`);
     next();
 });
 
 app.get('/test', (req, res, next) => {
     res.send('ok');
 });
+
