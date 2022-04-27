80559e37fad2ce55b17c68e21b570d0a398d8420
J1hu1Yang <86629716+J1hu1Yang@users.noreply.github.com>
2022-04-27 14:56:32 -0700

    add new styling to participants

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -16,23 +16,24 @@ import Viewer from './Viewer';
 import Participants from './Participants';
 import { Link, Navigate, useNavigate, useLocation } from "react-router-dom";
 
-
 import './App.css'
+import './room.css'
 function Room() {
   
+  const names = ['jammer1', 'jammer2', 'jammer3', 'jammer4', 'jammer5'];
+  const records = ['record1', 'record2', 'record3', 'record4', 'record5', 'record6'];
   //breaks the rendering 
   // const location = useLocation();
   // const { state: { guest, sessionID } = {} } = useLocation();
 
-  return (
-    <div>
-
-      <Participants></Participants>
+  return ( 
+    <div class="a0" >
+          <Participants></Participants>
       <Viewer></Viewer>
       <Recorder></Recorder>
-
       <Chat></Chat>
     </div>
   );
 }
 export default Room;
+
