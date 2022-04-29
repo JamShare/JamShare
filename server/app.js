673826adf95dcff826e593bb7183ce68f9a410a6
Devon Berry <devberry@pdx.edu>
2022-04-29 13:30:37 -0700

    Changed producer and consumer to array format

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 const http = require('http');
 const socket = require('socket.io');
 const port = process.env.PORT || 3001;
-const config = require('./serverConfig');
+const config = require('./mediasoupConfig');
 
 var chunks = [];
 
@@ -16,8 +16,8 @@ let worker;
 let server;
 let io;
 let app;
-let producer;
-let consumer;
+let producer = [];
+let consumer = [];
 let producerTransport;
 let consumerTransport;
 let mediasoupRouter;
@@ -150,19 +150,20 @@ async function runSocketServer() {
 
     socket.on('produce', async (data, callback) => {
       const { kind, rtpParameters } = data;
-      producer = await producerTransport.produce({ kind, rtpParameters });
-      callback({ id: producer.id });
+      producer.push(await producerTransport.produce({ kind, rtpParameters }));
+      console.log(producer[0].id)
+      callback({ id: producer[0].id });
 
       // inform clients about new producer
       socket.broadcast.emit('newProducer');
     });
 
     socket.on('consume', async (data, callback) => {
-      callback(await createConsumer(producer, data.rtpCapabilities));
+      callback(await createConsumer(producer[0], data.rtpCapabilities));
     });
 
     socket.on('resume', async (data, callback) => {
-      await consumer.resume();
+      await consumer[0].resume();
       callback();
     });
 
@@ -331,27 +332,23 @@ async function createConsumer(producer, rtpCapabilities) {
     return;
   }
   try {
-    consumer = await consumerTransport.consume({
+    consumer.push(await consumerTransport.consume({
       producerId: producer.id,
       rtpCapabilities,
       paused: producer.kind === 'video',
-    });
+    }));
   } catch (error) {
     console.error('consume failed', error);
     return;
   }
 
-  if (consumer.type === 'simulcast') {
-    await consumer.setPreferredLayers({ spatialLayer: 2, temporalLayer: 2 });
-  }
-
   return {
     producerId: producer.id,
-    id: consumer.id,
-    kind: consumer.kind,
-    rtpParameters: consumer.rtpParameters,
-    type: consumer.type,
-    producerPaused: consumer.producerPaused
+    id: consumer[0].id,
+    kind: consumer[0].kind,
+    rtpParameters: consumer[0].rtpParameters,
+    type: consumer[0].type,
+    producerPaused: consumer[0].producerPaused
   };
 }
 
