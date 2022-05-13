dd52b9b0e8dbf52faec1574a878e77af05b0f4c5
Qazzzie <santangelozach@gmail.com>
2022-05-13 16:33:32 -0700

    props.data in sessionID

--- client/src/components/JoinModal.js
+++ client/src/components/JoinModal.js
@@ -1,9 +1,36 @@
+import React, { useEffect, useState, useRef } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import handleSubmit from './Join.js';
+import CopyToClipboard from 'react-copy-to-clipboard';
 function JoinModal(props) {
+  const [copied, setCopied] = useState(false);
+  const [value, setValue] = useState('');
+  const inputArea = useRef(null);
+
+  function updateClipboard(newClip) {
+    navigator.clipboard.writeText(newClip).then(
+      () => {
+        setCopied("Copied!");
+      },
+      () => {
+        setCopied("Copy failed!");
+      }
+    );
+  }
+  
+  function copyLink() {
+    navigator.permissions
+      .query({ name: "clipboard-write" })
+      .then((result) => {
+        if (result.state === "granted" || result.state === "prompt") {
+          updateClipboard(inputArea.current?.innerText);
+        }
+      });
+  }
     return (
       <Modal {...props} aria-labelledby="contained-modal-title-vcenter"
         centered
@@ -18,15 +45,16 @@ function JoinModal(props) {
           <Container>
             <Row>
               <Col lg={4}></Col>
-              <Col lg={4}>
-                  room id here
-              </Col>
+                <Col lg={4} id="a" ref={inputArea} >
+                  {props.data}
+                </Col>
               <Col lg={4}></Col>
             </Row>
           </Container>
         </Modal.Body>
         <Modal.Footer>
-            <Button>Join Session</Button>
+            <Button onClick={copyLink}>copy to clipboard</Button>
+            <Button onClick={handleSubmit} >Join Session</Button>
             <Button onClick={props.onHide}>Close</Button>
         </Modal.Footer>
       </Modal>
