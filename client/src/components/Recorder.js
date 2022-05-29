8766cbf4211a77af828927f8e1d66af92eb44358
J1hu1Yang <86629716+J1hu1Yang@users.noreply.github.com>
2022-05-28 22:29:35 -0700

    update device choosing icon

--- client/src/components/Recorder.js
+++ client/src/components/Recorder.js
@@ -15,7 +15,7 @@ class Recorder extends React.Component {
         this.state = {
             isRecording: false,
             isPlaying: false,
-            icon: require('./assets/images/record.png'),
+            icon: require('./assets/images/device.jpg'),
             text: 'Jam!',
         };
         // const [image, setImage] = useState();
