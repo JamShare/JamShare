7ffb61551a6c107ca8449f4e0b90e7b777bb2c45
Devon Berry <devberry@pdx.edu>
2022-05-30 23:25:44 -0700

    fixed up morg signin

--- client/src/components/morg_signin.js
+++ client/src/components/morg_signin.js
@@ -1,8 +1,4 @@
-import React from "react";
-import {
-    Link,
-    Navigate
-} from "react-router-dom";
+import React, {Link} from "react";
 import axios from 'axios';
 import Cookies from "js-cookie";
 import {
@@ -15,82 +11,73 @@ import {
 const LINK_TO_SIGNUP = "/signup2"
 
 export class Morg_Signin extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                username: "",
-                password: "",
-            };
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-            this.handleChange = this.handleChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-        }
+    handleChange({
+        target
+    }) {
+        this.setState({
+            [target.name]: target.value
+        });
+    }
 
-        handleChange({
-            target
-        }) {
-            this.setState({
-                [target.name]: target.value
-            });
-        }
+    handleSubmit(event) {
+        axios.post('/auth/signin', {
+            username: this.state.username,
+            password: this.state.password,
+        }).then(res => {
+            if (res.data === true) {
+                alert(`Thanks for signing in ${this.state.username}`)
+                Cookies.set('username', this.state.username, {
+                    expires: 1
+                });
 
-        handleSubmit(event) {
-            axios.post('/auth/signin', {
-                username: this.state.username,
-                password: this.state.password,
-            }).then(res => {
-                if (res.data == true) {
-                    alert(`Thanks for signing in ${this.state.username}`)
-                    Cookies.set('username', this.state.username, {
-                        expires: 1
-                    });
-                
-                    Cookies.set('sessionID', Math.random().toString(36).substr(2, 9), {
-                        expires: 1
-                    });       
-                    
-                    alert(Cookies.get("sessionID"))
+                Cookies.set('sessionID', Math.random().toString(36).substr(2, 9), {
+                    expires: 1
+                });
 
-                    window.location.reload();
+                alert(Cookies.get("sessionID"))
+                window.location.reload();
+            } else {
+                alert('Unsuccessful login.');
+            }
 
-                } else
-                    alert('Unsuccessful login.');
+        }).catch(err => console.log(err));
+        event.preventDefault();
+    }
 
-            }).catch(err => console.log(err));
-
-
-            event.preventDefault();
-        }
-
-  render() {
-    /* if(typeof Cookies.get("sessionID") !== "undefined"){
-        return(<Navigate to="/" replace={true}/>);
-     }*/
-
-    return(
-        <Card >
-          <Form>
-            <Input
-              type="text"
-              name="username"
-              value={this.state.username}
-              placeholder="username"
-              onChange={this.handleChange}
-            />
-            <Input
-              type="text"
-              name="password"
-              value={this.state.password}
-              placeholder="password"
-              onChange={this.handleChange}
-            />
-            <Button onClick={this.handleSubmit}>Sign In</Button>
-          </Form>
-          <Link to={LINK_TO_SIGNUP}>Don't have an account?</Link>
-        </Card>
-      )
-  }
-
+    render() {
+        return (
+            <Card >
+                <Form>
+                    <Input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        placeholder="username"
+                        onChange={this.handleChange}
+                    />
+                    <Input
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        placeholder="password"
+                        onChange={this.handleChange}
+                    />
+                    <Button onClick={this.handleSubmit}>Sign In</Button>
+                </Form>
+                <Link to={LINK_TO_SIGNUP}>Don't have an account?</Link>
+            </Card>
+        )
+    }
 }
 
 export default Morg_Signin
