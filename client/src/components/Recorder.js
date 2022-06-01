75a482f69c457de61927c744c1da11927cde98ef
nkmurphy <nmurp2@pdx.edu>
2022-05-31 20:03:43 -0700

    Update Recorder.js

--- client/src/components/Recorder.js
+++ client/src/components/Recorder.js
@@ -175,9 +175,9 @@ function Recorder(props) {
         }
 
         recorderSource = recordContext.createMediaStreamTrackSource(obj.track);
-        recorderSource.connect(recorderNode); // potentially failing?
+        console.log("Recorder source: ", recorderSource, " obj.track: ", obj.track)
+        recorderSource.connect(recorderNode); 
 
-        //For now nick merge test
         recorderNode.parameters.get('isRecording').setValueAtTime(1, recordContext.currentTime);
         intervalReturn = setInterval(connectAudioBuffer, 1000); // connect an audio buffer every 1000ms
         playbackContext.resume(); // begin playback to users speakers and streamOut
